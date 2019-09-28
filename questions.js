314a46dc4b4eac9ca804fe844536d530aad80768
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-09-27 17:01:33 -0700

    Set up basic index.html set up, including a body container, title, quiz content section, and a next button

--- questions.js
+++ questions.js
@@ -3,11 +3,18 @@ var questions = [
       title: "Commonly used data types DO NOT include:",
       choices: ["strings", "booleans", "alerts", "numbers"],
       answer: "alerts"
+    //   answer = index 2
     },
     {
       title: "The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
+    //   answer = index 2
     },
+    {
+        title: "",
+        choices: [],
+        answer: ""
+    }
     ///etc.
   ];
